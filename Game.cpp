66d75d5dd3aee8640151c4538134e34cfd83dd0a
oaldsp <otaviopepe@alunos.utfpr.edu.br>
2023-11-09 18:47:04 -0300

    Depois da reuniao com monitor

--- Game.cpp
+++ Game.cpp
@@ -6,7 +6,7 @@ Game::Game():
 GC(&mE,&sE),
 pGrap(Graphics::getInstance()),
 background(CoordF(0.0f,0.0f), CoordF(1280,720)),
-p1(CoordF(300.f,250.f),true),
+p1(CoordF(300.f,150.f),true),
 c1(CoordF(50.f,270.f)),
 g1(CoordF(0.0f,300.0f), CoordF(600.0f,50.0f)),
 w1(CoordF(550.0f,250.0f), CoordF(50.0f,50.0f)),
